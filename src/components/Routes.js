91863ceab23781b3b42d74369121ed9a84f97c81
aliKatlabi <a2222k@gmail.com>
2024-08-11 21:14:15 +0200

    Update Routes.js
    committing to functional component

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -3,28 +3,6 @@ import { Route, Routes } from 'react-router-dom';
 import BookList from './BooksList';
 import SearchBooks from './Search';
 
-class AppRoutess extends React.Component  {
-    constructor(props) {
-        super(props);
-       
-    }
-    render(){
-        return (
-            <Routes>
-              <Route
-                path="/"
-                element={<BookList books={this.props.books} onShelfChange={this.props.onShelfChange} />}
-              />
-              <Route
-                path="/search"
-                element={<SearchBooks books={this.props.books} onShelfChange={this.props.onShelfChange} />}
-              />
-            </Routes>
-          );
-    }
-  
-}
-
 const AppRoutes = (props)=>{
 
   return (
